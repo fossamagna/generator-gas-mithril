6db16504628b6ddb6ffa20dccf7b8793fa026c04
MURAKAMI Masahiko <m-murakami@esm.co.jp>
2016-08-26 23:08:49 +0900

    Use gas-run,gas-api-run modules.

--- generators/app/templates/client/scripts/gas-client.js
+++ generators/app/templates/client/scripts/gas-client.js
@@ -1,10 +1,10 @@
 if (process.env.NODE_ENV === 'local') {
   const config = require('gas-rest-config');
-  const Client = require('./google-apps-script-rest-client');
+  const Client = require('gas-api-run').default;
   const client = new Client(config[process.env.NODE_ENV]);
   global.init = client.init.bind(client);
   module.exports = client;
 } else {
-  const Client = require('./google-apps-script-run');
+  const Client = require('gas-run').default;
   module.exports = new Client();
 }
