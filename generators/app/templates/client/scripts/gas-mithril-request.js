639c6f684b3c39f7a9c8343441927ca2c0644d71
MURAKAMI Masahiko <m-murakami@esm.co.jp>
2017-02-27 14:13:21 +0900

    Migrate to mithril 1.0.1

--- generators/app/templates/client/scripts/gas-mithril-request.js
+++ generators/app/templates/client/scripts/gas-mithril-request.js
@@ -3,16 +3,40 @@
 const m = require('mithril');
 const client = require('./gas-client');
 
-module.exports = (...args) => {
-  m.startComputation();
-  const deferred = m.deferred();
-  client.run(...args)
-  .then(value => {
-    deferred.resolve(value);
-    m.endComputation();
-  }).catch(e => {
-    deferred.reject(e);
-    m.endComputation();
-  });
-  return deferred.promise;
+let oncompletion = m.redraw;
+
+function setCompletionCallback(callback) {
+  oncompletion = callback;
 }
+
+function finalizer() {
+  let count = 0;
+  function complete() {
+    if (--count === 0 && typeof oncompletion === "function") {
+      oncompletion();
+    }
+  }
+
+  return function finalize(promise) {
+    const then = promise.then;
+    promise.then = function() {
+      count++;
+      const next = then.apply(promise, arguments);
+      next.then(complete, function(e) {
+        complete();
+        if (count === 0) {
+          throw e;
+        }
+      });
+      return finalize(next);
+    }
+    return promise;
+  }
+}
+
+function request(...args) {
+  const finalize = finalizer();
+  return finalize(client.run(...args));
+}
+
+module.exports = {request, setCompletionCallback}
--- generators/app/templates/client/scripts/main.js
+++ generators/app/templates/client/scripts/main.js
@@ -1,22 +1,24 @@
 'use strict';
 
 const m = require('mithril');
-const request = require('./gas-mithril-request');
+const prop = require('mithril/stream');
+const request = require('./gas-mithril-request').request;
 
 const app = {
-  controller: function () {
-    const ctl = this;
-    this.message = m.prop('');
-    this.result = m.prop();
-    this.handleAuthClick = () => {
-      request('echo', ctl.message()).then(message => {
-        ctl.result(message);
-      }).catch(e => {
-        ctl.result(e);
-      });
-    };
+  oninit: function (vnode) {
+    vnode.state.message = prop('');
+    vnode.state.result = prop();
   },
-  view: ctl => {
+  handleAuthClick: function () {
+    const state = this;
+    request('echo', state.message()).then(message => {
+      state.result(message);
+    }).catch(e => {
+      state.result(e);
+    });
+  },
+  view: vnode => {
+    const ctl = vnode.state;
     return <div class="row">
             <form class="form-inline">
               <div class="form-group">
@@ -24,7 +26,7 @@ const app = {
                   placeholder="Echo Message"
                   onchange={m.withAttr('value', ctl.message)} value={ctl.message()}/>
               </div>{' '}
-              <button type="button" class="btn btn-default" onclick={ctl.handleAuthClick}>Echo</button>
+              <button type="button" class="btn btn-default" onclick={ctl.handleAuthClick.bind(ctl)}>Echo</button>
             </form>
             <p>{ctl.result()}</p>
            </div>;
